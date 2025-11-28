09c1cd98c2dc1df66ee2656b04e09aec4e8a8770
SATYAM RAJPUT <satyam974y@gmail.com>
2025-11-28 22:00:27 +0530

    Time: 3 ms (33.45%), Space: 45.9 MB (46.26%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum =  prices[0] + prices[1];
+        if(sum<money){
+            return(money-sum);
+        }else if(money<sum){
+            return(money);
+        }
+        return 0;
+    }
+}
