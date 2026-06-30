ad3e0fe54a6fa5a16d0b74203ce50ced6929be30
SATYAM RAJPUT <satyam974y@gmail.com>
2026-06-30 14:11:04 +0530

    Time: 1 ms (96.07%), Space: 45 MB (13.74%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -3,17 +3,17 @@ class Solution {
         char arr[] = s.toCharArray();
         int n = arr.length;
 
-        if(n<k){
-            int l = 0;
-            int r = n-1;
-            while(l<r){
-                char temp = arr[l];
-                arr[l] = arr[r];
-                arr[r]= temp;
-                l++;
-                r--;
-            }
-        }else if(n<2*k && n>=k || n>=2*k){
+        // if(n<k){
+        //     int l = 0;
+        //     int r = n-1;
+        //     while(l<r){
+        //         char temp = arr[l];
+        //         arr[l] = arr[r];
+        //         arr[r]= temp;
+        //         l++;
+        //         r--;
+        //     }
+        // }else if(n<2*k && n>=k || n>=2*k){
             for(int i = 0;i<arr.length;i += 2*k){
                 int l = i;
                 int r = Math.min(i+k - 1 ,arr.length-1);
@@ -25,7 +25,7 @@ class Solution {
                     r--;
                 }
             }
-        }
+        // }
         String str = new String(arr);
         return str;
     }    
