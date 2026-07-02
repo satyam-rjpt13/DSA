287a373885dded05086b38dd3f38e68a471764e5
SATYAM RAJPUT <satyam974y@gmail.com>
2026-07-02 14:18:08 +0530

    Time: 1 ms (96.57%), Space: 42.7 MB (36.29%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        long i =1 ;
+        while(i<n){
+            i*=2;
+        }
+        if (i==n){
+            return true;
+        }
+        return false;
+    }
+}
